f65eae45e42ea13df80184b1b0f8cdd8735ff05e
Chiranjeeb Nayak <chiranjeebnayak.37@gmail.com>
2021-08-14 00:30:08 +0530

    news api fetch

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+
+
+function navbar() {
+
+
+    
+    return (
+        <>
+
+        </>
+    )
+}
+
+export default navbar
